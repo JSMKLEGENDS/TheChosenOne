1806233e9f3bd29ab4209c639c6a9de84ca60611
JSMKLEGENDS <jensenstanleyking66@gmail.com>
2026-06-09 09:11:09 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "La Liga 38-0-0!",
         // Activity type number (0 = Playing).
         type: 0, 
       },
